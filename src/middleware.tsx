fcec24c5801945b7374b9983fd439c8f0fcf7a1f
Jake Darby <designdarby@gmail.com>
2024-07-18 13:21:25 +0100

    added middleware

---
+++ src/middleware.tsx
@@ -0,0 +1,18 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+//I want to tell Clerk what routes are protected
+const isProtectedRoute = createRouteMatcher(["/user(.*)"]);
+
+//to define public and protected routes
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+//the matcher is finding a a correlation bewteen your routes and the routes you specified in clerkMiddleware()
+//the matcher is written using regex
+//regular expressions are used to find matches in patterns
+//find matches where the route starts with / and don't filter anything that comes after. Find all matches.
+//! Don't change this line
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
